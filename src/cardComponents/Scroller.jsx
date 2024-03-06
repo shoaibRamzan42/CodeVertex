f7b2eeae9a904f88b7d1994dcc734a44575659a8
shoaibRamzan42 <adnanrmzan420@gmail.com>
2024-03-06 18:47:50 +0500

    updated

--- src/cardComponents/Scroller.jsx
+++ src/cardComponents/Scroller.jsx
@@ -26,7 +26,7 @@ const ScCard=()=>{
       
       <div id="portfolio" className="flex bg-[#F5F4F4] overflow-scroll no-scrollbar w-full justify-between items-center "  >
       <Carousel    transition={{ duration: 1 }}
-        autoplay={{ delay: 2000, reverseDirection: false, loop: true }}
+        // autoplay={{ delay: 2000, reverseDirection: false, loop: true }}
           className="rounded-xl"
   navigation={({ setActiveIndex, activeIndex, length }) => (
     <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 z-50 flex gap-2">
